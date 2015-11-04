c830e4774b728817fdbd4fe6c78f5aeea85ef747
shsong97 <shsong97@gmail.com>
2015-11-04 02:33:38 +0000

    heroku setting static files

--- kids/settings.py
+++ kids/settings.py
@@ -82,7 +82,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.dirname(os.path.realpath(__file__))
+STATIC_ROOT = 'staticfiles' # os.path.dirname(os.path.realpath(__file__))
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
