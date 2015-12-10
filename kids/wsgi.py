9241a95cabdc57e90567750ed97b3e96a039c2a4
shsong97 <shsong97@gmail.com>
2015-12-10 02:39:22 +0000

    update 12/10

--- kids/wsgi.py
+++ kids/wsgi.py
@@ -13,9 +13,5 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kids.settings")
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 
-#heroku settings
-#from dj_static import Cling
-#application = Cling(get_wsgi_application())
-
 from whitenoise.django import DjangoWhiteNoise
 application = DjangoWhiteNoise(application)
